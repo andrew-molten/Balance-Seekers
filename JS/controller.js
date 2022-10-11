b5350d25cb856cc199f08f16f6b4c33288004ed5
andrewmolten <andrewmolten23@gmail.com>
2022-10-11 14:21:17 +1300

    insert markup for activities

--- JS/controller.js
+++ JS/controller.js
@@ -1,6 +1,10 @@
 const activityInput = document.querySelector(".add__activity__input");
 const addButton = document.querySelector(".add__btn");
 const addActivity = document.querySelector("add__activity");
+const clearButton = document.querySelector(".clear_btn");
+const activitiesDisplay = document.querySelector(".activities_display");
+const upButton = document.querySelector(".push_up_btn");
+const downButton = document.querySelector(".push_down_btn");
 
 // class Activity {
 //   constructor(activityName) {
@@ -19,21 +23,46 @@ class App {
 
     // Event Handlers
     addButton.addEventListener("click", this._processActivity.bind(this));
+
+    //Can't get the below button to work - cannot read property of null (reading 'addeventlistener')
+    // upButton.addEventListener("click", this._moveActivityUp.bind(this));
+
+    // addButton.addEventListener("click", this.reset());
   }
 
+  // Processing Activity
   _processActivity(e) {
     e.preventDefault();
     const activity = activityInput.value;
     this.#clearInputField();
-    this.#activities.push(activity);
-    this._setLocalStorage();
+    // this.#activities.push(activity);
+    this._render(activity);
+
     console.log(this.#activities);
+  }
+
+  _generateMarkup(activity) {
+    return `
+    <li>${activity}</li>
+        <button class="btn done_btn">🔥</button>
+        <button class="btn push_up_btn">↑</button>
+        <button class="btn push_down_btn">↓</button>
+        <button class="btn edit_btn">Edit</button>
+    `;
+  }
+
+  _render(activity) {
+    const markup = this._generateMarkup(activity);
+    this.#activities.push(markup);
+    this._setLocalStorage();
+    activitiesDisplay.insertAdjacentHTML("afterbegin", markup);
   }
 
   #clearInputField() {
     this.#parentEl.querySelector(".add__activity__input").value = "";
   }
 
+  //Storage
   _setLocalStorage() {
     localStorage.setItem("activities", JSON.stringify(this.#activities));
   }
@@ -45,11 +74,20 @@ class App {
 
     this.#activities = data;
 
-    this.#activities.forEach((activity) => console.log(activity));
+    console.log(this.#activities);
+
+    this.#activities.forEach((activity) =>
+      activitiesDisplay.insertAdjacentHTML("afterbegin", activity)
+    );
   }
 
   reset() {
     localStorage.removeItem("activities");
+  }
+
+  // Adjusting Activities
+  _moveActivityUp() {
+    console.log("up up up");
   }
 }
 
