bcdc61188b0d4edf9cd37d5fbe562f59020f2057
andrewmolten <andrewmolten23@gmail.com>
2022-10-26 14:06:04 +1300

    moveActivity, submitForm, addsubItem,processAddSub

--- JS/controller.js
+++ JS/controller.js
@@ -34,10 +34,10 @@ class App {
     activitiesDisplay.addEventListener("click", (e) => {
       e.preventDefault();
       if (e.target.closest(".push_up_btn")) {
-        this._moveActivity(e, "up");
+        this._moveActivityUpOrDown(e, "up");
       }
       if (e.target.closest(".push_down_btn")) {
-        this._moveActivity(e, "down");
+        this._moveActivityUpOrDown(e, "down");
       }
       if (e.target.closest(".delete_btn")) {
         this._deleteActivity(e);
@@ -69,16 +69,17 @@ class App {
   }
 
   _generateMarkup(activity, id) {
-    const subCategories = [];
+    let subCategories = "";
 
     /////////////////////////////////////////////// NEED TO MAKE THE BELOW CODE WORK!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-    // activity.variation
-    //   ? activity.variation.forEach((element) =>
-    //       subCategories.push(`<li class="sub_item">${input}</li>`)
-    //     )
-    //   : "";
-    // console.log(activity, subCategories);
+    activity.variation
+      ? activity.variation.forEach(
+          (element) =>
+            (subCategories =
+              subCategories + `<li class="sub_item">${element.type}</li>`)
+        )
+      : "";
 
     return `
     <li class="activity_item" id="id${id}">${activity.activity} 
@@ -89,12 +90,11 @@ class App {
         <button class="btn delete_btn">Delete</button>
         <button class="btn add_sub_btn">+</button>
         <ol class="sub_category">
-        ${``}
+        ${subCategories}
         </ol>
         </li>
     `;
   }
-  // ${!activity.activity.variation}
 
   _storeIDAndRender() {
     this._setIDs(this.#activities);
@@ -115,8 +115,16 @@ class App {
     let id = -1;
 
     arr.forEach((el) => {
+      let variationIdDecimal = -1;
       id = id + 1;
       el.id = id;
+      if (el.variation.length === 0) return;
+      // Set ID for any variations
+      el.variation.forEach((va) => {
+        variationIdDecimal = variationIdDecimal + 1;
+        const variationID = id + "." + variationIdDecimal;
+        va.id = +variationID;
+      });
     });
   }
 
@@ -168,7 +176,7 @@ class App {
   }
 
   // Adjusting Activities
-  _moveActivity(e, direction) {
+  _moveActivityUpOrDown(e, direction) {
     // console.log(e.target.parentElement.parentElement);
     const movingID = +e.target.closest(".activity_item").id.slice(2);
     if (movingID === this.#activities.length - 1 && direction === "up") return;
@@ -177,9 +185,13 @@ class App {
     const newID = direction === "up" ? movingID + 1 : movingID - 1;
     const element = this.#activities[movingID];
 
-    this.#activities.splice(movingID, 1);
-    this.#activities.splice(newID, 0, element);
+    this._moveActivity(movingID, newID, element);
     this._storeIDAndRender();
+  }
+
+  _moveActivity(oldID, newID, element) {
+    this.#activities.splice(oldID, 1);
+    this.#activities.splice(newID, 0, element);
   }
 
   _deleteActivity(e) {
@@ -222,19 +234,11 @@ class App {
   }
 
   _processAddSub(e, subCat, itemID) {
+    if (input === "") return;
     const addSubInput = document.querySelector(".add_sub_act_input");
     const input = addSubInput.value;
-    if (input === "") return;
     this.#activities[itemID].variation.push({ type: input });
-    this._setLocalStorage();
-    // NEED TO assign id's, and fix the HTML insertion so it happens with the render
-    // Possibly see if these functions can be simplified
-    subCat.innerHTML = "";
-    subCat.insertAdjacentHTML(
-      "beforebegin",
-      `<li class="sub_item">${input}</li>`
-    );
-    console.log(this.#activities);
+    this._storeIDAndRender();
   }
 
   _submitForm(e) {
@@ -242,17 +246,18 @@ class App {
     const date = sessionDate.value;
     const length = sessionLength.value;
     const id = logSessionToID;
+    const element = this.#activities[id];
     sessionDate.value = "";
     sessionLength.value = "";
-    const session = [date, length];
-    this.#activities[id].sessions.push(session);
-    this._setLocalStorage();
+    const session = {
+      date: date,
+      length: length,
+    };
+    element.sessions.push(session);
+    this._moveActivity(id, 0, element);
+    this._storeIDAndRender();
     console.log(this.#activities[id]);
-    // document.querySelector(".log_session_form").style.display = "hidden";
-    // console.log("Anyone there");
     this._closeLogSessionForm();
-    // ^^^^ Why is this function not working
-    // console.log(id);
   }
 }
 
